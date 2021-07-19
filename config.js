ea555f72cc7530cb6479643240492d43b81560bd
BEARmei <87651857+BEARmei@users.noreply.github.com>
2021-07-19 23:34:33 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hê luu cậu!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'okii',
+    title: 'Cậu thích tớ phải ko ? 😙',
+    desc: 'Nếu cậu ko trả lời mà thoát ra tức là muốn làm ny tớ rùi đó nha :3 ',
+    btnYes: 'Yêu ơi là yêu <33',
+    btnNo: 'Cậu mơ à!:3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại nhìn cậu đáng yêu vl =)))',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều lắm 😘😘',
+    messDesc: 'Tối nay 7h tớ đưa cậu đi chơi nhé công chúa.Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo :3',
+    btnAccept: 'Okii <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
